7963ece3b1e842a88182f6b1d711855db331295e
dapatayaya@gmail.com <dapatayaya@gmail.com>
2025-06-03 07:31:48 +0700

    Commit pertama

---
+++ app/Http/Controllers/KaegoriController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kaegori;
+use App\Http\Requests\StoreKaegoriRequest;
+use App\Http\Requests\UpdateKaegoriRequest;
+
+class KaegoriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKaegoriRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Kaegori $kaegori)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Kaegori $kaegori)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKaegoriRequest $request, Kaegori $kaegori)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Kaegori $kaegori)
+    {
+        //
+    }
+}
